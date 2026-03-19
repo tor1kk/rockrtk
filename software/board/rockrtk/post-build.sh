3654c7d8d6221a9228910fc8cd45e961e1e4c247
tor1kk <vsobeskiy@gmail.com>
2026-03-19 21:05:33 +0200

    add LTE mm config

--- software/board/rockrtk/post-build.sh
+++ software/board/rockrtk/post-build.sh
@@ -4,3 +4,9 @@ BOARD_DIR="$(dirname $0)"
 cp $BOARD_DIR/uEnv.txt $BINARIES_DIR/uEnv.txt
 
 install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
+
+# NM keyfiles must be root-owned and mode 600
+chmod 600 $TARGET_DIR/etc/NetworkManager/system-connections/*.nmconnection 2>/dev/null || true
+
+# SSH authorized_keys must be mode 600
+chmod 600 $TARGET_DIR/root/.ssh/authorized_keys 2>/dev/null || true
--- software/external.mk
+++ software/external.mk
@@ -1,1 +1,1 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_ROCKRTK_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_ROCKRTK_BUILDROOT_PATH)/package/*/*.mk))
